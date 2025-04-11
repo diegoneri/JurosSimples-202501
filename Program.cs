6d4e0eb675e88aac8b7973a4ba837aa0e51ff2e2
Diego Neri <diego.neri@etec.sp.gov.br>
2025-04-11 20:03:50 -0300

    Finaliza Juros Simples

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+decimal capital, juros, montante;
+double taxa;
+int tempo;
+
+Console.Clear();
+Console.WriteLine("Juros simples (j)\n");
+
+Console.Write("Capital [c] (R$).......: ");
+capital = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Taxa de juros [i] (%)..: ");
+taxa = Convert.ToDouble(Console.ReadLine());
+Console.Write("Tempo [t] (meses)......: ");
+tempo = Convert.ToInt32(Console.ReadLine());
+
+juros = capital * Convert.ToDecimal(taxa / 100) * tempo;
+montante = capital + juros;
+
+Console.WriteLine($"\nJuros (R$).....: {juros:N2}");
+Console.WriteLine($"Montante (R$)..: {montante:N2}");
